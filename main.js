8be2cdf580f53e4559bd3ef2153c9b3defbe2bb9
Nehemias Balsas <brandonbalsas7@gmail.com>
2023-07-17 23:11:23 -0300

    .

--- main.js
+++ main.js
@@ -1,77 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const tabla = document.getElementById("tabla1");
+  const celdas = tabla.getElementsByTagName("td");
+  const mensaje = document.getElementById("mensaje");
+  const resetBtn = document.getElementById("reset");
 
-    const tabla = document.getElementById("tabla1");
-    const celdas = tabla.getElementsByTagName("td");
-    const mensaje = document.getElementById("mensaje");
-    const resetBtn = document.getElementById("reset");
-    
-    let JugadorActual = "X";
-    let JuegoTerminado = false;
-    let movimientos = 0;
+  let JugadorActual = "X";
+  let JuegoTerminado = false;
+  let movimientos = 0;
 
-    resetBtn.addEventListener("click", iniciarJuego);
+  resetBtn.addEventListener("click", iniciarJuego);
 
-    function iniciarJuego() {
+  function iniciarJuego() {
     JugadorActual = "X";
     JuegoTerminado = false;
     movimientos = 0;
     mensaje.textContent = "";
     for (let i = 0; i < celdas.length; i++) {
-    celdas[i].textContent = "";
-    celdas[i].addEventListener("click", hacerMovimiento);
+      celdas[i].textContent = "";
+      celdas[i].removeEventListener("click", hacerMovimiento);
+      celdas[i].addEventListener("click", hacerMovimiento);
     }
-}
+  }
 
-    function hacerMovimiento() {
-        if (this.textContent === "" && !JuegoTerminado) {
-        const image = document.createElement("img");
-        image.classList.add("imagen-jugador");
-        image.src = JugadorActual === "X" ? "imagenes/x.png" : "imagenes/o.png";
+  function hacerMovimiento() {
+    if (this.textContent === "" && !JuegoTerminado) {
+      const image = document.createElement("img");
+      image.classList.add("imagen-jugador");
+      image.src = JugadorActual === "X" ? "x.png" : "o.png";
 
-        this.appendChild(image);
-        movimientos++;
-        VerificarGanador();
-        JugadorActual = JugadorActual === "X" ? "O" : "X";
-        }
+      this.appendChild(image);
+      movimientos++;
+      VerificarGanador();
+      JugadorActual = JugadorActual === "X" ? "O" : "X";
     }
+  }
 
-
-    function VerificarGanador() {
+  function VerificarGanador() {
     const combosGanadores = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8], // Filas
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8], // Columnas
-    [0, 4, 8],
-    [2, 4, 6], // Diagonales
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // Filas
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // Columnas
+      [0, 4, 8],
+      [2, 4, 6], // Diagonales
     ];
 
     for (let combo of combosGanadores) {
-    const [a, b, c] = combo;
-    if (
+      const [a, b, c] = combo;
+      if (
         celdas[a].textContent === JugadorActual &&
         celdas[b].textContent === JugadorActual &&
         celdas[c].textContent === JugadorActual
-    ) {
+      ) {
         FinDelJuego(`¡El jugador ${JugadorActual} ha ganado!`);
         return;
-    }
+      }
     }
 
     if (movimientos === 9) {
-    FinDelJuego("¡Empate!");
+      FinDelJuego("¡Empate!");
     }
-}
+  }
 
-    function FinDelJuego(msg) {
+  function FinDelJuego(msg) {
     JuegoTerminado = true;
     mensaje.textContent = msg;
     for (let i = 0; i < celdas.length; i++) {
-    celdas[i].removeEventListener("click", hacerMovimiento);
+      celdas[i].removeEventListener("click", hacerMovimiento);
     }
-}
+  }
 
-    iniciarJuego();
+  iniciarJuego();
 });
