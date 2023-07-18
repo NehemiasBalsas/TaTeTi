e2a3ab803eb555ba8f475ebddfb3f1cba818fd74
Nehemias Balsas <brandonbalsas7@gmail.com>
2023-07-17 23:16:22 -0300

    ,

--- main.js
+++ main.js
@@ -35,34 +35,34 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function VerificarGanador() {
-    const combosGanadores = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8], // Filas
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8], // Columnas
-      [0, 4, 8],
-      [2, 4, 6], // Diagonales
-    ];
+function VerificarGanador() {
+  const combosGanadores = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Filas
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columnas
+    [0, 4, 8],
+    [2, 4, 6], // Diagonales
+  ];
 
-    for (let combo of combosGanadores) {
-      const [a, b, c] = combo;
-      if (
-        celdas[a].textContent === JugadorActual &&
-        celdas[b].textContent === JugadorActual &&
-        celdas[c].textContent === JugadorActual
-      ) {
-        FinDelJuego(`¡El jugador ${JugadorActual} ha ganado!`);
-        return;
-      }
-    }
-
-    if (movimientos === 9) {
-      FinDelJuego("¡Empate!");
+  for (let combo of combosGanadores) {
+    const [a, b, c] = combo;
+    if (
+      celdas[a].classList.contains(JugadorActual) &&
+      celdas[b].classList.contains(JugadorActual) &&
+      celdas[c].classList.contains(JugadorActual)
+    ) {
+      FinDelJuego(`¡El jugador ${JugadorActual} ha ganado!`);
+      return;
     }
   }
+
+  if (movimientos === 9) {
+    FinDelJuego("¡Empate!");
+  }
+}
 
   function FinDelJuego(msg) {
     JuegoTerminado = true;
